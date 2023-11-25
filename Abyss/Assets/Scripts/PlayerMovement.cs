0be59ea4acc7a911dd66f83fbea1f14af7ffca8e
VinDEV99 <adelucchi1999@yahoo.com>
2023-11-25 03:54:25 -0800

    Wall Slide and Wall Jump
    Implemented basic wall slide and wall jump to the player controller.
    Needs tweaking in the future.
    Added basic visual to show direction character is facing.
    Also added package related to VS as it was missing.

--- Abyss/Assets/Scripts/PlayerMovement.cs
+++ Abyss/Assets/Scripts/PlayerMovement.cs
@@ -6,26 +6,47 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 15f;  // Adjust this to set the player's movement speed
     public float jumpForce = 20f; // Adjust this to set the player's jump force
-    public float jumpHoldDuration = 0.5f;
+    public float jumpHoldDuration = 0.5f; //how long the player is holding the jump button
     public float dashSpeed = 30f; // Adjust this to set the dash speed
     public float dashDuration = 1f; // Adjust this to set the dash duration
     public float dashCooldown = 0.5f; // Adjust this to set the dash cooldown
+    public float wallSlidingSpeed = 2f; //speed at which player slides down walls
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(100f, 30f);
 
     private Rigidbody2D rb;
+
     private bool isGrounded;
     private bool isDashing;
+    private bool isWallSliding = false;
     private float originalMoveSpeed;
     private bool canDash = true;
+
+    //ground and wall checks
     private Transform groundCheck;
     private LayerMask groundLayer;
+    private Transform wallCheck;
+    private LayerMask wallLayer;
 
     private float lastGrounded;
+
+    private float moveInput;
+    private bool isFacingRight = true;
+
+    private Vector2 move;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         groundCheck = transform.Find("GroundCheck"); // Create an empty GameObject called "GroundCheck" slightly below the player
         groundLayer = LayerMask.GetMask("Ground"); // Make sure your ground objects are on the "Ground" layer
+        wallCheck = transform.Find("WallCheck"); //Check for wall slide/jump
+        wallLayer = LayerMask.GetMask("Ground"); //uses the ground layer for now
         originalMoveSpeed = moveSpeed;
     }
 
@@ -34,9 +55,6 @@ public class PlayerMovement : MonoBehaviour
         // Check if the player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
-        // Player input for movement
-        float moveInput = Input.GetAxis("Horizontal");
-        Vector2 move = new Vector2(moveInput, 0f);
 
         // Apply dash if not currently dashing
         if (canDash && Input.GetButtonDown("Fire1"))
@@ -44,17 +62,32 @@ public class PlayerMovement : MonoBehaviour
             StartCoroutine(Dash());
         }
 
-        // If dashing, move with dash speed
-        if (isDashing)
-        {
-            rb.gravityScale = 0;
-            rb.velocity = new Vector2(move.x * dashSpeed, rb.velocity.y);
+        WallSlide();
+        WallJump();
+
+        if (!isWallJumping) {
+            FlipCharSprite();
         }
-        else
-        {
+
+    }
+
+    private void FixedUpdate() {
+        // Player input for movement
+        moveInput = Input.GetAxis("Horizontal");
+        if (!isWallJumping) {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+            //move = new Vector2(moveInput, 0f);
+        }
+
+        // If dashing, move with dash speed
+        if (isDashing) {
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(moveInput * dashSpeed, rb.velocity.y);
+        }
+        else {
             // Otherwise, move with regular speed
             rb.gravityScale = 4;
-            rb.velocity = new Vector2(move.x * moveSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
         }
 
 
@@ -62,15 +95,74 @@ public class PlayerMovement : MonoBehaviour
 
         // Player input for jumping
 
-        if (Input.GetButton("Jump") && Time.time < lastGrounded + jumpHoldDuration)
-        {
+        if (Input.GetButton("Jump") && Time.time < lastGrounded + jumpHoldDuration) {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
 
-        /*if (isGrounded && Input.GetButton("Jump"))
-        {
-            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }*/
+    private bool IsWalled() {
+
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide() {
+        if (IsWalled() && !isGrounded && moveInput != 0f) {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        } else {
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump() {
+        if (isWallSliding) {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        } else {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f) {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection) {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                if (isFacingRight) {
+                    localScale.x = 0.7f;
+                }
+                else {
+                    localScale.x = -0.7f;
+                }
+
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping() {
+        isWallJumping = false;
+    }
+
+    private void FlipCharSprite() {
+        if (isFacingRight && moveInput < 0f || !isFacingRight && moveInput > 0f) {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            if (isFacingRight) {
+                localScale.x = 0.7f;
+            } else {
+                localScale.x = -0.7f;
+            }
+            
+            transform.localScale = localScale;
+        }
     }
 
     private System.Collections.IEnumerator Dash()
