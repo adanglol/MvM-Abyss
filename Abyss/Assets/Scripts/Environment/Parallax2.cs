4b4d14a0e33f0c78b917a38f348f9213da044900
VinDEV99 <adelucchi1999@yahoo.com>
2023-12-04 14:43:19 -0800

    LevelLoader Update
    It has some warnings but should work for now. In order to add a scene, instead of build index, you will have to provide a name, as well as a spawn location for the player in the next scene.
    
    There is a bug at the moment where the player is invisible for a few seconds but I think it should be able to be worked out eventually, and isn't game breaking atm.

--- Abyss/Assets/Scripts/Environment/Parallax2.cs
+++ Abyss/Assets/Scripts/Environment/Parallax2.cs
@@ -7,8 +7,18 @@ public class Parallax2 : MonoBehaviour
     public Transform cam;
     public float relativeMove = .3f;
 
+    private void Awake() {
+        if (cam == null) {
+            cam = GameObject.Find("Virtual Camera").transform;
+        }
+    }
+
     void Update()
     {
+        if (cam == null) {
+            cam = GameObject.Find("Virtual Camera").transform;
+        }
+        
         transform.position = new Vector2(cam.position.x * relativeMove, cam.position.y * relativeMove);
     }
 }
--- Abyss/Assets/Scripts/LevelLoader.cs
+++ Abyss/Assets/Scripts/LevelLoader.cs
@@ -6,21 +6,36 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     private GameObject player;
+    private GameObject outputLocation;
 
-    public int Scene;
-
+    public Scene previousScene;
     
+    public string sceneName;
+    public string outputLocationName;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         Debug.Log("Found " + player);
+        previousScene = SceneManager.GetActiveScene();
+        SceneManager.SetActiveScene(previousScene);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Check");
         if (other.transform == player.transform) {
-            SceneManager.LoadScene(Scene);
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            previousScene = SceneManager.GetActiveScene();
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        previousScene = SceneManager.GetActiveScene();
+        outputLocation = GameObject.Find(outputLocationName);
+        player.transform.position = outputLocation.transform.position;
+        Debug.Log("Previous scene was: " + previousScene.name);
+        SceneManager.UnloadSceneAsync(previousScene);
     }
 }
